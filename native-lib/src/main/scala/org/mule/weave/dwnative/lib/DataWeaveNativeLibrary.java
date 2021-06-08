c979d599a10770391b0036c9783a8a923054ca8f
Mariano De Achaval <mdeachaval@salesforce.com>
2021-06-08 17:18:37 -0300

    Simple lib

--- native-lib/src/main/scala/org/mule/weave/dwnative/lib/DataWeaveNativeLibrary.java
+++ native-lib/src/main/scala/org/mule/weave/dwnative/lib/DataWeaveNativeLibrary.java
@@ -4,53 +4,20 @@ import org.graalvm.nativeimage.IsolateThread;
 import org.graalvm.nativeimage.c.function.CEntryPoint;
 import org.graalvm.nativeimage.c.type.CCharPointer;
 import org.graalvm.nativeimage.c.type.CTypeConversion;
-import org.mule.weave.dwnative.NativeRuntime;
-import org.mule.weave.dwnative.WeaveExecutionResult;
-import org.mule.weave.dwnative.utils.DataWeaveUtils;
-import org.mule.weave.v2.parser.ast.variables.NameIdentifier;
-import org.mule.weave.v2.runtime.ScriptingBindings;
-
-import java.io.File;
-import java.util.concurrent.Executors;
+import org.mule.weave.v2.parser.MappingParser;
+import org.mule.weave.v2.parser.ast.structure.DocumentNode;
+import org.mule.weave.v2.parser.phase.ParsingResult;
+import org.mule.weave.v2.parser.phase.PhaseResult;
+import org.mule.weave.v2.sdk.ParsingContextFactory;
+import org.mule.weave.v2.sdk.WeaveResource;
 
 
 public class DataWeaveNativeLibrary {
 
-    private static NativeRuntime runtime = new NativeRuntime(DataWeaveUtils.getCacheHome(), DataWeaveUtils.getLibPathHome(), new File[0], Executors.newCachedThreadPool());
-
-    @CEntryPoint(name = "runDW")
-    public static CCharPointer runDW(IsolateThread thread, CCharPointer transform) {
+    @CEntryPoint(name = "compilePeregrineExpression")
+    public static CCharPointer compilePeregrineExpression(IsolateThread thread, CCharPointer transform) {
         final String script = CTypeConversion.toJavaString(transform);
-        final WeaveExecutionResult run = runtime.run(script, NameIdentifier.ANONYMOUS_NAME().toString(), new ScriptingBindings());
-        return CTypeConversion.toCString(run.result()).get();
+        final PhaseResult<ParsingResult<DocumentNode>> parse = MappingParser.parse(MappingParser.parsingPhase(), WeaveResource.anonymous(script), ParsingContextFactory.createParsingContext());
+        return CTypeConversion.toCString("{ \"hasErrors\": " + parse.hasErrors() + "}").get();
     }
-
-    @CEntryPoint(name = "runDW1")
-    public static CCharPointer runDW1(IsolateThread thread, CCharPointer transform, CCharPointer inputName, CCharPointer input) {
-        final String script = CTypeConversion.toJavaString(transform);
-        final String inputNameStr = CTypeConversion.toJavaString(inputName);
-        final String inputContentStr = CTypeConversion.toJavaString(input);
-
-        final ScriptingBindings scriptingBindings = new ScriptingBindings();
-        scriptingBindings.addBinding(inputNameStr, inputContentStr);
-
-        final WeaveExecutionResult run = runtime.run(script, NameIdentifier.ANONYMOUS_NAME().toString(), scriptingBindings);
-        return CTypeConversion.toCString(run.result()).get();
-    }
-
-    @CEntryPoint(name = "runDW2")
-    public static CCharPointer runDW2(IsolateThread thread, CCharPointer transform, CCharPointer inputName, CCharPointer input, CCharPointer inputName2, CCharPointer input2) {
-        final String script = CTypeConversion.toJavaString(transform);
-        final String inputNameStr = CTypeConversion.toJavaString(inputName);
-        final String inputContentStr = CTypeConversion.toJavaString(input);
-
-        final String inputNameStr2 = CTypeConversion.toJavaString(inputName2);
-        final String inputContentStr2 = CTypeConversion.toJavaString(input2);
-        final ScriptingBindings scriptingBindings = new ScriptingBindings();
-        scriptingBindings.addBinding(inputNameStr, inputContentStr);
-        scriptingBindings.addBinding(inputNameStr2, inputContentStr2);
-        final WeaveExecutionResult run = runtime.run(script, NameIdentifier.ANONYMOUS_NAME().toString(), scriptingBindings);
-        return CTypeConversion.toCString(run.result()).get();
-    }
-
 }
