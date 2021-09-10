3329b235897fc6beac7b654b9aeb859f15d8bf11
TomoDC <ch.chibana@gmail.com>
2021-09-10 16:26:45 -0300

    DW-731: Clean up

--- native-lib/src/main/c/DataWeaveNative.c
+++ native-lib/src/main/c/DataWeaveNative.c
@@ -2,42 +2,21 @@
 #include <stdio.h>
 #include <time.h>
 
-#include <graal_isolate.h>
-
-
-/* C function that gets passed to Java as a function pointer. */
-void c_print(int success, char* cstr) {
-  printf("Compilation: %d %s\n",success , cstr);
-}
+#include "libDw.h"
 
 int main(int argc, char** argv) {
-//  compilation_result result;
-  int i = 1;
-//  char* expression = "attributes.headers.myParam";
-  while (i)
-  {
-    graal_isolatethread_t* thread;
-    if (graal_create_isolate(NULL, NULL, &thread)) {
-      return -1;
-    }
-    printf("Start Compilation TEO!!!!\n");
-//    compile(thread, expression,  &c_print);
-//    printf("Done\n");
-
-//    printf("Start Compilation\n");
-//    compileSync(thread, expression,  &result);
-//    free(&result)
-//    freeCompilationResult(thread, &result);
-
-    printf("Done\n");
-    //
-
-    // This frees all memory allocated by the GraalVM runtime
-    if (graal_tear_down_isolate(thread) != 0) {
-       fprintf(stderr, "shutdown error\n");
-       return 1;
-     }
-//     free(thread);
-   }
-    return 0;
+  graal_create_isolate_params_t isolate_params = {};
+  graal_isolate_t* isolate;
+  graal_isolatethread_t* thread;
+  if (graal_create_isolate(&isolate_params, &isolate, &thread)) {
+    return -1;
+  }
+  clock_t start, end;
+  double time_taken;
+  start = clock();
+  printf("%s\n", runDW(thread, "%dw 2.0 import Dependency from dw::deps::Deps \n @Dependency(artifactId = '142ebe1a-6e25-4670-9220-0cdce791f1b8:string-utils-weave-module:1.0.0-SNAPSHOT')\n import times from org::mule::commons::StringUtils\n output application/json --- times('Test', 2)"));
+  end = clock();
+  time_taken = ((double) (end - start)) / CLOCKS_PER_SEC;
+  printf("fun() took %f seconds to execute \n", time_taken);
+  return 0;
 }
---
+++ native-lib/src/main/scala/org/mule/weave/dwnative/lib/DataWeaveNativeLibrary.java
@@ -0,0 +1,56 @@
+package org.mule.weave.dwnative.lib;
+
+import org.graalvm.nativeimage.IsolateThread;
+import org.graalvm.nativeimage.c.function.CEntryPoint;
+import org.graalvm.nativeimage.c.type.CCharPointer;
+import org.graalvm.nativeimage.c.type.CTypeConversion;
+import org.mule.weave.dwnative.NativeRuntime;
+import org.mule.weave.dwnative.WeaveExecutionResult;
+import org.mule.weave.dwnative.utils.DataWeaveUtils;
+import org.mule.weave.v2.parser.ast.variables.NameIdentifier;
+import org.mule.weave.v2.runtime.ScriptingBindings;
+
+import java.io.File;
+import java.util.concurrent.Executors;
+
+
+public class DataWeaveNativeLibrary {
+
+    private static NativeRuntime runtime = new NativeRuntime(DataWeaveUtils.getCacheHome(), DataWeaveUtils.getLibPathHome(), new File[0], Executors.newCachedThreadPool());
+
+    @CEntryPoint(name = "runDW")
+    public static CCharPointer runDW(IsolateThread thread, CCharPointer transform) {
+        final String script = CTypeConversion.toJavaString(transform);
+        final WeaveExecutionResult run = runtime.run(script, NameIdentifier.ANONYMOUS_NAME().toString(), new ScriptingBindings());
+        return CTypeConversion.toCString(run.result()).get();
+    }
+
+    @CEntryPoint(name = "runDW1")
+    public static CCharPointer runDW1(IsolateThread thread, CCharPointer transform, CCharPointer inputName, CCharPointer input) {
+        final String script = CTypeConversion.toJavaString(transform);
+        final String inputNameStr = CTypeConversion.toJavaString(inputName);
+        final String inputContentStr = CTypeConversion.toJavaString(input);
+
+        final ScriptingBindings scriptingBindings = new ScriptingBindings();
+        scriptingBindings.addBinding(inputNameStr, inputContentStr);
+
+        final WeaveExecutionResult run = runtime.run(script, NameIdentifier.ANONYMOUS_NAME().toString(), scriptingBindings);
+        return CTypeConversion.toCString(run.result()).get();
+    }
+
+    @CEntryPoint(name = "runDW2")
+    public static CCharPointer runDW2(IsolateThread thread, CCharPointer transform, CCharPointer inputName, CCharPointer input, CCharPointer inputName2, CCharPointer input2) {
+        final String script = CTypeConversion.toJavaString(transform);
+        final String inputNameStr = CTypeConversion.toJavaString(inputName);
+        final String inputContentStr = CTypeConversion.toJavaString(input);
+
+        final String inputNameStr2 = CTypeConversion.toJavaString(inputName2);
+        final String inputContentStr2 = CTypeConversion.toJavaString(input2);
+        final ScriptingBindings scriptingBindings = new ScriptingBindings();
+        scriptingBindings.addBinding(inputNameStr, inputContentStr);
+        scriptingBindings.addBinding(inputNameStr2, inputContentStr2);
+        final WeaveExecutionResult run = runtime.run(script, NameIdentifier.ANONYMOUS_NAME().toString(), scriptingBindings);
+        return CTypeConversion.toCString(run.result()).get();
+    }
+
+}
