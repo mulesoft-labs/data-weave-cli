408e3ecacbc11dcfc6959728f0c8d70a70ccc1f9
mlischetti <lischettim@gmail.com>
2022-04-04 14:26:53 -0300

    W-10942122: Flag spells commands as experimental

--- native-cli/src/main/scala/org/mule/weave/dwnative/cli/commands/AddWizardCommand.scala
+++ native-cli/src/main/scala/org/mule/weave/dwnative/cli/commands/AddWizardCommand.scala
@@ -3,6 +3,8 @@ package org.mule.weave.dwnative.cli.commands
 import org.mule.weave.dwnative.cli.Console
 import org.mule.weave.dwnative.cli.utils.SpellsUtils
 
+import scala.io.StdIn
+
 class AddWizardCommand(config: CloneWizardConfig, console: Console) extends WeaveCommand {
 
   private val utils = new SpellsUtils(console)
@@ -13,14 +15,21 @@ class AddWizardCommand(config: CloneWizardConfig, console: Console) extends Weav
     val wizardName = if (wizard == null) "DW" else wizard
     val wizardFolder = utils.grimoireFolder(wizard)
     if (wizardFolder.exists()) {
-      console.error(s"Wizard `${wizard}` was already added.")
+      console.error(s"Wizard `$wizard` was already added.")
       -1
     } else {
-      console.info(s"Fetching `$wizardName's` Grimoire.")
-      val url: String = utils.buildRepoUrl(wizard)
-      val processBuilder = new ProcessBuilder("git", "clone", url, wizardFolder.getAbsolutePath)
-      processBuilder.inheritIO()
-      processBuilder.start().waitFor()
+      console.warn(s"You are adding `$wizardName's` Grimoire, are you sure? [y/n]")
+      val trustWizard = StdIn.readBoolean()
+      if (trustWizard) {
+        console.info(s"Fetching `$wizardName's` Grimoire.")
+        val url: String = utils.buildRepoUrl(wizard)
+        val processBuilder = new ProcessBuilder("git", "clone", url, wizardFolder.getAbsolutePath)
+        processBuilder.inheritIO()
+        processBuilder.start().waitFor()
+      } else {
+        console.warn(s"Wizard `$wizardName' was not added.")
+        0
+      }
     }
   }
 }
--- native-cli/src/main/scala/org/mule/weave/dwnative/cli/commands/UsageCommand.scala
+++ native-cli/src/main/scala/org/mule/weave/dwnative/cli/commands/UsageCommand.scala
@@ -28,17 +28,19 @@ class UsageCommand(console: Console) extends WeaveCommand {
       |
       | --prop or -p       | Property to be passed.
       | --input or -i      | Declares a new input.
-      | --list-spells      | List all the available spells
-      | --spell or -s      | Runs a spell. Use the <spellName> or <wizard>/<spellName> for spells from a given wizard.
-      | --update-grimoires | Update all wizard grimoires
-      | --add-wizard       | Downloads wizard grimoire so that its spell are accessible
-      | --local-spell      | Executes a local folder spell
-      | --new-spell        | Create a new spell
       | --output or -o     | Specifies output file for the transformation if not standard output will be used.
-      | --file or -f       | Path to the file
-      | --eval             | Evaluates the script instead of writing it
-      | --version          | The version of the CLI and Runtime
+      | --file or -f       | Path to the file.
+      | --eval             | Evaluates the script instead of writing it.
+      | --version          | The version of the CLI and Runtime.
       | --verbose or -v    | Enable Verbose Mode.
+      | --list-spells      | [Experimental] List all the available spells.
+      | --spell or -s      | [Experimental] Runs a spell. Use the <spellName> or <wizard>/<spellName> for spells from a given wizard.
+      | --local-spell      | [Experimental] Executes a local folder spell.
+      | --new-spell        | [Experimental] Create a new spell.
+      | --add-wizard       | [Experimental] Downloads wizard grimoire so that its spell are accessible.
+      | --remove-wizard    | [Experimental] Remove a wizard from your local repository.
+      | --update-grimoires | [Experimental] Update all wizard grimoires.
+      |
       |
       | Example:
       |
